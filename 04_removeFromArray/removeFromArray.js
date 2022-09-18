db03a53c023a70bfd7dab95fa65fa66bf48cdaba
RealPerro <chr1@hey.com>
2022-09-18 13:27:39 +0100

    solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(theArray, ...theArgs) {
+    for (const thisArg of theArgs) {
+        theArray = theArray.filter(function(value, index, arr){
+            return value !== thisArg});
+    }
+    return theArray;
 };
 
 // Do not edit below this line
