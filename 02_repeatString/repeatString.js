35f381d70196f8dd27dc7ec0169238b0e96d0790
RealPerro <chr1@hey.com>
2022-09-18 11:02:55 +0100

    solve 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    num < 0 ? newWord = "ERROR" : newWord = word.repeat(num);
+    return newWord;
 };
 
 // Do not edit below this line
