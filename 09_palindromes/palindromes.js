d5849ef85ddc73f1e54ae5284004d2461e92a949
RealPerro <chr1@hey.com>
2022-09-18 19:09:57 +0100

    solves 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    newText = text.toLowerCase();
+    newText = newText.replace(/[\W]/gi, "");
+    return reverseString(newText) === newText;
 };
 
+const reverseString = function(string) {
+    let newString = "";
+    string = string.toString()
+    for (let i = string.length-1; i > -1; i--) {
+        newString += string[i];
+    } 
+    return newString
+    };
 // Do not edit below this line
 module.exports = palindromes;
