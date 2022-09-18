3969352b4006fb54f1c6318391b0e4d15783b43c
RealPerro <chr1@hey.com>
2022-09-18 19:21:42 +0100

    pass 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {return "OOPS";}
+    let fibArray = [0,1];
+    for ( let i = 2; i <= n; i++ ) {
+        fibArray.push(fibArray[i-2] + fibArray[i-1]);
+    }
+    return fibArray[n];
 };
 
 // Do not edit below this line
