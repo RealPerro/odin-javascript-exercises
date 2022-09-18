24f57327dd7a81c85e7749c5cfcfe608cfd117a8
RealPerro <chr1@hey.com>
2022-09-18 14:47:18 +0100

    pass 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(F) {
+  return Math.round((F - 32) * 50 / 9) / 10 ;
 };
 
-const ctof = function() {
-
+const ctof = function(C) {
+  return Math.round ((C * 90 / 5) + 320) / 10;
 };
 
 // Do not edit below this line
