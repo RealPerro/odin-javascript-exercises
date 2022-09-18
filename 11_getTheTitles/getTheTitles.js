3bd6e3ece6d463142fb48f52d366ce2b27e61460
RealPerro <chr1@hey.com>
2022-09-18 20:00:44 +0100

    pass 11 titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+out = []
+const getTheTitles = function(hash) {
+    for (b in hash) {out.push(hash[b]["title"])}
+    return out;
 };
 
 // Do not edit below this line
