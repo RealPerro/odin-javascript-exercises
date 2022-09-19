15e87f3d563f12e8dba25658b28c200c2ed56e08
RealPerro <chr1@hey.com>
2022-09-19 10:03:31 +0100

    solve 12 find oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
-};
+let findTheOldest = function(hash) {
+    let result = [];
+    let oldest = 0;
+    const currentYear = new Date().getFullYear();
+    for (b in hash) {
+        hash[b]["yearOfDeath"] = hash[b]["yearOfDeath"] ? 
+                                    hash[b]["yearOfDeath"] : currentYear;
+        let thisAge = hash[b]["yearOfDeath"] - hash[b]["yearOfBirth"];
+        (hash[b]["yearOfDeath"] && hash[b]["yearOfBirth"]) ?
+                                    hash[b]["age"] = thisAge : hash[b]["age"] = 0;
+        if (hash[oldest]["age"] < thisAge) {oldest = b;};  
+                    };
+    return hash[oldest];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
